6cdb774e330df0a4893e1e3dcd171da09770cc9f
Grant Peterson <dgrantpete@gmail.com>
2023-04-01 20:52:35 -0600

    Renamed tests so it works with pytest

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from site_reader import LetterInfo, WordleGame, WordDict, Feedback, WordInfo
 def test_generate_feedback():
     test_answer = "aaabb"
     
-    answer_matches_expected = lambda guess, expected_feedback: str(WordInfo(WordleGame.generate_full_feedback(test_answer, guess))) == expected_feedback
+    answer_matches_expected = lambda guess, expected_feedback: str(WordInfo(WordleGame.generate_full_feedback(guess, test_answer))) == expected_feedback
 
     assert answer_matches_expected("aabbb", "AA~BB")
     assert answer_matches_expected("bbaaa", "bbAaa")
@@ -12,7 +12,3 @@ def test_generate_feedback():
     assert answer_matches_expected("baaaa", "bAAa~")
     assert answer_matches_expected("bbbba", "b~~Ba")
     assert answer_matches_expected("acaca", "A~A~a")
-
-
-if __name__ == "__main__":
-    test_generate_feedback()
